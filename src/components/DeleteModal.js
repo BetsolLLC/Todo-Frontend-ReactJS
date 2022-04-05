a70ad5a17534ebeb9c7250b00a64d1e41dc4f3b7
Rakshitha Lakshmi <Rakshitha.Lakshmi@betsol.com>
2022-04-06 02:15:59 +0530

    removal and finishing

--- src/components/DeleteModal.js
+++ src/components/DeleteModal.js
@@ -1,11 +1,34 @@
 function DeleteModal(props) {
+  function confirmHandler() {
+    const url = "http://localhost:5000/api/v1/";
+    const deleteData = {
+      id: props.deleteId
+    };
+    fetch(url, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(deleteData)
+    }).then((response) => {
+      if(!response.ok) {
+        alert('Delete failed');
+        props.closeModal();
+      } else {
+        props.getItems();
+      }
+    });
+  }
+
+  function cancelHandler() {
+    props.closeModal();
+  }
+
   return (
     <div className="modal">
       <p>Are you sure?</p>
-      <button className="btn btn--alt">
+      <button className="btn btn--alt" onClick={confirmHandler}>
         Confirm
       </button>
-      <button className="btn">
+      <button className="btn" onClick={cancelHandler}>
         Cancel
       </button>
     </div>
