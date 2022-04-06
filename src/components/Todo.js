47063753b4e4d630bc4a290db7ee99fa612d38a9
Rakshitha Lakshmi <Rakshitha.Lakshmi@betsol.com>
2022-04-06 09:16:31 +0530

    todo with backdrop

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,11 +6,26 @@ import Backdrop from "./Backdrop";
 function Todo(props) {
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
 
+  function completeItem() {
+    const url = "";
+    const completeItem = { id: props.id };
+    fetch(url, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(completeItem),
+    }).then((response) => {
+      if (!response.ok) {
+        alert("Completing task failed");
+      }
+      props.getItems();
+    });
+  }
+  
   return (
     <div className="card">
       <div>
         {
-          <input type="checkbox"/>
+          <input type="checkbox" onClick={completeItem}/>
         }
         <p>{props.title}</p>
       </div>
